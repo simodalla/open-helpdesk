e267912a208cc111304b683967774bcdd200d427
Simone Dalla <simodalla@gmail.com>
2014-05-29 10:16:59 +0200

    add custom validation for 'tipologies' field on AdminTicket form

--- helpdesk/forms.py
+++ helpdesk/forms.py
@@ -10,12 +10,17 @@ from mezzanine.conf import settings
 
 class TicketAdminForm(forms.ModelForm):
     def clean_tipologies(self):
+        """
+        Additional validation for 'tipologies' field. If the number of
+        tipologies selected is greater than 'HELPDESK_TICKET_MAX_TIPOLOGIES'
+        setting, raise an ValidationError
+        """
         settings.use_editable()
         max_tipologies = settings.HELPDESK_TICKET_MAX_TIPOLOGIES
         tipologies = self.cleaned_data['tipologies']
         if len(tipologies) > max_tipologies:
             msg = _('Too many tipologies selected. You can select a maximum'
-                    ' of %(max)s')
+                    ' of %(max)s.')
             raise ValidationError(msg, code='too_many_tipologies',
                                   params={'max': max_tipologies})
         return self.cleaned_data['tipologies']
