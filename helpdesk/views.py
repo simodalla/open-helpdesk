2f9ea4804f95f6f1e039344dc2b209a16714d3ba
Simone Dalla <simodalla@gmail.com>
2014-05-29 21:29:25 +0200

    use of self.request into OpenTicketView

--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-# from django.contrib import messages
+from django.contrib import messages
 from django.contrib.admin.templatetags.admin_urls import admin_urlname
 from django.core.urlresolvers import reverse
 from django.views.generic import RedirectView
@@ -20,23 +20,18 @@ class OpenTicketView(GroupRequiredMixin, RedirectView):
                       settings.HELPDESK_ADMINS]
     permanent = False
 
-    def get(self, request, *args, **kwargs):
-        kwargs.update({'request_user': request.user})
-        return super(OpenTicketView, self).get(request, *args, **kwargs)
-
     def get_redirect_url(self, *args, **kwargs):
         ticket_pk = kwargs.get('pk')
         ticket_changelist_url = reverse(admin_urlname(Ticket._meta,
                                                       'changelist'))
         try:
             ticket = Ticket.objects.get(pk=ticket_pk)
-            ticket.open(kwargs.get('request_user'))
-            # ticket.save()
-            # TODO: messaggio di successo
+            ticket.open(self.request.user)
+            messages.success(self.request, 'Profile details updated.')
         except Ticket.DoesNotExist:
             # TODO: messaggio di errore per ticket non trovato
             return ticket_changelist_url
-        except ValueError as ve:
+        except ValueError:
             # TODO: messaggio di errore per open che ha lanciato un'eccezione
             return ticket_changelist_url
         return reverse(admin_urlname(Ticket._meta, 'change'),
