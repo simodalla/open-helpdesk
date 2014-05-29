d59da3d8863f71fce0a4f115e6519043ddbe2eb4
Simone Dalla <simodalla@gmail.com>
2014-05-29 22:57:25 +0200

    add comments and fix pep8

--- helpdesk/views.py
+++ helpdesk/views.py
@@ -6,12 +6,11 @@ from django.contrib.admin.templatetags.admin_urls import admin_urlname
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import RedirectView
-
 from braces.views import GroupRequiredMixin
-
 from mezzanine.conf import settings
 
 from .models import Ticket
+
 
 settings.use_editable()
 
@@ -23,23 +22,21 @@ class OpenTicketView(GroupRequiredMixin, RedirectView):
 
     def get_redirect_url(self, *args, **kwargs):
         ticket_pk = kwargs.get('pk')
-        ticket_changelist_url = reverse(admin_urlname(Ticket._meta,
-                                                      'changelist'))
+        url_on_error = reverse(admin_urlname(Ticket._meta, 'changelist'))
         error_msg_prefix = _('An error occurs.')
         try:
             ticket = Ticket.objects.get(pk=ticket_pk)
             ticket.open(self.request.user)
             msg = _('Ticket n.%(pk)s is opened and assigned.') % {
                 'pk': ticket_pk}
-            messages.success(self.request, msg)
         except Ticket.DoesNotExist:
             msg = _('Ticket n.%(pk)s does not exist.') % {'pk': ticket_pk}
             messages.error(self.request, '{} {}'.format(error_msg_prefix, msg))
-            return ticket_changelist_url
-        except ValueError as ve:
+            return url_on_error
+        except ValueError as ve:  # Errors raised by ticket.open
             messages.error(self.request, '{} {}'.format(error_msg_prefix,
                                                         str(ve)))
-            return ticket_changelist_url
+            return url_on_error
+        messages.success(self.request, msg)
         return reverse(admin_urlname(Ticket._meta, 'change'),
                        args=(ticket_pk,))
-
