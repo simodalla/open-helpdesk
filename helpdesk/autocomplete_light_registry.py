94e2d87553792c85736367b3d8cdd845757eeeef
Simone Dalla <simodalla@gmail.com>
2014-06-09 18:32:36 +0200

    add classes and registrations for add 'requester' to autocomplete fields

--- helpdesk/autocomplete_light_registry.py
+++ helpdesk/autocomplete_light_registry.py
@@ -3,8 +3,12 @@ from __future__ import unicode_literals, absolute_import
 
 from django.utils.translation import ugettext_lazy as _
 import autocomplete_light
+from mezzanine.utils.models import get_user_model
+from mezzanine.conf import settings
 
 from .models import Ticket, HelpdeskUser
+
+User = get_user_model()
 
 
 class TicketAutocomplete(autocomplete_light.AutocompleteModelBase):
@@ -23,4 +27,11 @@ class TicketAutocomplete(autocomplete_light.AutocompleteModelBase):
     def choice_label(self, choice):
         return "n.{} [{}]".format(choice.id, choice.get_clean_content(10))
 
+
+class RequesterAutocomplete(autocomplete_light.AutocompleteModelBase):
+    search_fields = ['username', 'last_name', 'first_name', 'email']
+    choices = User.objects.filter(groups__name=settings.HELPDESK_REQUESTERS)
+
+
 autocomplete_light.register(Ticket, TicketAutocomplete)
+autocomplete_light.register(User, RequesterAutocomplete)
