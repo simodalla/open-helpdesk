9eb7ae2ffea0a4d151e7cc73807c883ce886273c
Simone Dalla <simodalla@gmail.com>
2014-06-10 14:54:15 +0200

    add pytest ixture for test stdout and stderr of django custom commands

--- conftest.py
+++ conftest.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 import os
+
+import pytest
+
 from django.conf import settings
+
+
+from six.moves import cStringIO
 
 
 def pytest_configure():
     if not settings.configured:
         os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+
+
+@pytest.fixture(scope="module")
+def stringios():
+    return cStringIO(), cStringIO()
