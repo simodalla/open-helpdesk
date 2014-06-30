01e00f99203d2a6057a7f7ba91008645452a31a1
Simone Dalla <simodalla@gmail.com>
2014-06-30 17:37:38 +0200

    refactor of format_ticket_message tag and relatives tests

--- helpdesk/templatetags/helpdesk_tags.py
+++ helpdesk/templatetags/helpdesk_tags.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals, absolute_import
 
 from django import template
+from django.core.exceptions import ObjectDoesNotExist
 
 from helpdesk.models import Report, Message
 
@@ -12,12 +13,18 @@ register = template.Library()
 def format_ticket_message(message, **kwargs):
     if not issubclass(message.__class__, Message):
         raise TypeError("{} isn't subclass object of".format(message, Message))
-    context = {'css_style': None, 'message': message,
+    context = {'css_style': '', 'message': message,
                'css_class': 'form-row',
                'model': getattr(message._meta, 'model_name',
                                 message._meta.module_name)}
     if 'css_class' in kwargs:
         context.update({'css_class': kwargs['css_class']})
-    if isinstance(message, Report):
-        context['css_style'] = 'text-align: right;'
+    try:
+        if message.report:
+            context.update({'model': getattr(
+                message.report._meta, 'model_name',
+                message.report._meta.module_name),
+                'css_style': 'text-align: right;'})
+    except ObjectDoesNotExist:
+        pass
     return context
--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -3,8 +3,6 @@ from __future__ import unicode_literals, absolute_import
 
 import pytest
 
-from django.utils.timezone import now
-
 from helpdesk.templatetags.helpdesk_tags import format_ticket_message
 from helpdesk.models import Message, Report
 
@@ -14,10 +12,14 @@ def message():
     obj = Message()
     return obj
 
+
 @pytest.fixture
-def report():
+def report(operator):
     obj = Report()
-    return obj
+    obj.content = "bla"
+    obj.sender = operator
+    obj.save()
+    return Message.objects.get(pk=obj.pk)
 
 
 class TestFormatTicketTag(object):
@@ -50,13 +52,21 @@ class TestFormatTicketTag(object):
     def test_css_style_item_in_dict_returned(self, message):
         context = format_ticket_message(message)
         assert 'css_style' in context.keys()
-        assert context['css_style'] is None
+        assert context['css_style'] == ''
 
+    @pytest.mark.django_db
     def test_css_style_item_in_dict_returned_and_setted_if_report_obj(
             self, report):
         context = format_ticket_message(report)
         assert 'css_style' in context.keys()
         assert 'text-align: right' in context['css_style']
+
+    @pytest.mark.django_db
+    def test_report_model_item_in_dict_returned(self, report):
+        context = format_ticket_message(report)
+        assert 'model' in context.keys()
+        assert context['model'] == getattr(
+            Report._meta, 'model_name', Report._meta.module_name)
 
     def test_css_class_item_in_dict_returned_and_default_value(self, message):
         context = format_ticket_message(message)
