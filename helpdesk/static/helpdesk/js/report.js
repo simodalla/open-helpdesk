d392850ccca2bae25566ed3e45912ee29156750e
Simone Dalla <simodalla@gmail.com>
2014-07-17 16:07:59 +0200

    add client pending interactions

--- helpdesk/static/helpdesk/js/report.js
+++ helpdesk/static/helpdesk/js/report.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    var pending_action = "put_on_pending";
+    var input_action_on_ticket = "input[name='action_on_ticket']";
+    var content_field = "#id_content";
 
     ['a.add-another', '.autocomplete-light-widget .remove'].forEach(
         function (elem) { $(elem).remove(); }
@@ -6,4 +9,64 @@ $(document).ready(function () {
     ['_addanother', '_continue'].forEach(
         function (elem) { $("input[name='" + elem + "']").remove(); }
     );
+
+    //
+    if ($(input_action_on_ticket+':checked').val() == pending_action) {
+        $("#fieldset_pending_range_data").show();
+    }
+
+    $("#id_end_pending_date").datepicker({
+        showOtherMonths: true,
+        selectOtherMonths: true,
+        changeMonth: true,
+        changeYear: true,
+        minDate: -0,
+        maxDate: "+2Y",
+        dateFormat: "dd/mm/yy",
+        appendText: " (dd/mm/yy)",
+        onClose: function (dateText, inst) {
+            if (dateText.trim().length > 0) {
+                $(this).removeClass("ui-state-highlight");
+            }
+        }
+    });
+
+    $("#alert_for_empty_content").dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            "Ok": function () {
+                $(this).dialog("close");
+                $(content_field).focus();
+            }
+        },
+        close: function (event, ui) {
+            $(content_field).focus();
+        }
+    });
+
+    $(content_field).focusout(function () {
+        var input = $(this);
+        if (input.val().trim().length > 0) {
+            input.removeClass("ui-state-highlight");
+        } else {
+            input.addClass("ui-state-highlight");
+        }
+    });
+
+    $(input_action_on_ticket).click(function() {
+        if ($(this).val() == pending_action) {
+            $("#fieldset_pending_range_data").show("slow");
+            if ($(content_field).val().trim().length == 0) {
+                $("#alert_for_empty_content").dialog("open");
+                [content_field, '#id_end_pending_date'].forEach(
+                    function (elem) {
+                        $(elem).addClass("ui-state-highlight");
+                    }
+                );
+            }
+        } else {
+            $("#fieldset_pending_range_data").hide("slow");
+        }
+    });
 });
