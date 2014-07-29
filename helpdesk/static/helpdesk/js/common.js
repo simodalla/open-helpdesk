24f2c5c914face9ab6e8ce61ce0c544d5cb203ae
Simone Dalla <simodalla@gmail.com>
2014-07-29 09:26:08 +0200

    refactor javascript system

--- helpdesk/static/helpdesk/js/common.js
+++ helpdesk/static/helpdesk/js/common.js
@@ -1,20 +1,25 @@
-requirejs.config({
-    baseUrl: '/static/helpdesk/js/lib',
-    paths: {
-        app: '../app'
-    },
-    map: {
-      // '*' means all modules will get 'jquery'
-      // for their 'jquery' dependency.
-      '*': { 'jquery': 'jquery' },
+if('__helpdeskStaticAppPath' in window) {
+    requirejs.config({
+        baseUrl: __helpdeskStaticAppPath + '/lib',
+        paths: {
+            app: '..'
+        },
+        map: {
+          // '*' means all modules will get 'jquery'
+          // for their 'jquery' dependency.
+          '*': { 'jquery': 'jquery' },
 
-      // 'jquery' wants the real jQuery module
-      // though. If this line was not here, there would
-      // be an unresolvable cyclic dependency.
-      'jquery': { 'jquery': 'jquery' }
-    }
-});
+          // 'jquery' wants the real jQuery module
+          // though. If this line was not here, there would
+          // be an unresolvable cyclic dependency.
+          'jquery': { 'jquery': 'jquery' }
+        }
+    });
 
-define(['jquery'], function (jq) {
-    return jq.noConflict( true );
-});
+    define(['jquery'], function (jq) {
+        return jq.noConflict( true );
+    });
+} else {
+    alert('A system error is occurs. Contact the system administrator.');
+    window.location.replace('/admin/');
+}
