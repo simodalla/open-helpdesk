d93f420239b9efeae65d2d715a62e29e31c2829e
Simone Dalla <simodalla@gmail.com>
2014-08-02 11:38:39 +0200

    attempt to fix jenkins

--- tests/settings_postgres.py
+++ tests/settings_postgres.py
@@ -15,7 +15,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pytest_django' + db_suffix,
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'USER': 'postgres',
     },
 }
