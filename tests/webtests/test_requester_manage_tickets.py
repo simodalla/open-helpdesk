21ba64f320372ba688a2011d6794cbd9632a4118
Simone Dalla <simodalla@gmail.com>
2014-08-02 16:48:25 +0200

    refactor webtest for use plain WebTest, nor py.test

--- tests/webtests/test_requester_manage_tickets.py
+++ tests/webtests/test_requester_manage_tickets.py
@@ -2,78 +2,58 @@
 from __future__ import unicode_literals, absolute_import
 
 import pytest
+
 from django.contrib.admin.templatetags.admin_urls import admin_urlname
 from django.core.urlresolvers import reverse
 
+from django_webtest import WebTest
+
 from openhelpdesk.models import Ticket, PRIORITY_NORMAL
 
-
-pytestmark = pytest.mark.django_db
-
-
-@pytest.fixture
-def add_form_data(tipologies, ticket_content):
-    class FormData(object):
-        def __init__(self):
-            self.content = ticket_content
-            self.priority = PRIORITY_NORMAL
-            self.tipologies = [t.pk for t in tipologies[0:2]]
-    return FormData()
-
-@pytest.fixture(scope='class')
-def add_url(request):
-    setattr(request.cls, 'url', reverse(admin_urlname(Ticket._meta, 'add')))
+from ..conftest import get_tipologies
 
 
-@pytest.mark.usefixtures('add_url')
-class TestAddingTicketByRequester(object):
-    def test_requester_field_is_setted_with_current_logged_user(
-            self, app, requester, add_form_data):
-        response = app.get(self.url, user=requester)
+class AddFormData(object):
+    def __init__(self, content="Foo", tipologies=None, priority=None):
+        self.content = content
+        self.priority = priority or PRIORITY_NORMAL
+        self.tipologies = [t.pk for t in tipologies] if tipologies else []
+
+
+@pytest.mark.usefixtures('requester_cls')
+class TestAddingTicketByRequester(WebTest):
+
+    def setUp(self):
+        self.url = reverse(admin_urlname(Ticket._meta, 'add'))
+        self.content = "Foo"
+        self.tipologies = get_tipologies(2)
+        self.add_form_data = AddFormData(self.content, self.tipologies)
+
+    def test_requester_field_is_setted_with_current_logged_user(self):
+        response = self.app.get(self.url, user=self.user)
         form = response.forms['ticket_form']
-        form['content'] = add_form_data.content
-        form['priority'] = add_form_data.priority
-        form['tipologies'] = add_form_data.tipologies
+        form['content'] = self.add_form_data.content
+        form['priority'] = self.add_form_data.priority
+        form['tipologies'] = self.add_form_data.tipologies
         form.submit('_save')
         ticket = Ticket.objects.latest()
-        assert ticket.requester.pk == requester.pk
-        assert ticket.priority == PRIORITY_NORMAL
-        assert ticket.content in ticket.content
-        assert (set(ticket.tipologies.values_list('pk', flat=True))
-                == set(add_form_data.tipologies))
+        self.assertEqual(ticket.requester_id, self.user.pk)
+        self.assertEqual(ticket.priority, PRIORITY_NORMAL)
+        self.assertIn('Foo', ticket.content)
+        self.assertSetEqual(
+            set(ticket.tipologies.values_list('pk', flat=True)),
+            set(self.add_form_data.tipologies))
 
-    def test_statuschangelog_obj_is_created(
-            self, app, requester, add_form_data):
-        response = app.get(reverse(admin_urlname(Ticket._meta, 'add')),
-                           user=requester)
+    def test_statuschangelog_obj_is_created(self):
+        response = self.app.get(self.url, user=self.user)
         form = response.forms['ticket_form']
-        form['content'] = add_form_data.content
-        form['priority'] = add_form_data.priority
-        form['tipologies'] = add_form_data.tipologies
-        form.submit('_continue')
-        assert Ticket.objects.count() == 1
+        form['content'] = self.add_form_data.content
+        form['priority'] = self.add_form_data.priority
+        form['tipologies'] = self.add_form_data.tipologies
+        form.submit('_save')
         ticket = Ticket.objects.latest()
-        assert ticket.status_changelogs.count() == 1
+        self.assertEqual(ticket.status_changelogs.count(), 1)
         statuschangelog = ticket.status_changelogs.all()[0]
-        assert statuschangelog.before == ''
-        assert statuschangelog.after == Ticket.STATUS.new
-        assert statuschangelog.changer.pk == requester.pk
-
-
-class TestChangingTicketByRequester(object):
-    def test_set_sender_field_on_change_ticket_inserting_message_obj(
-            self, app, opened_ticket):
-        """
-        Test for testing custom "save_formset" method of TicketAdmin. From
-        ticket change view is added a new message object and "save_formset"
-        set sender field of message object to request.user
-        """
-        assert len(opened_ticket.messages.all()) == 0
-        response = app.get(
-            reverse('admin:openhelpdesk_ticket_change', args=(opened_ticket.pk,)),
-            user=opened_ticket.requester)
-        form = response.forms['ticket_form']
-        form['messages-0-content'] = 'foo'
-        form.submit('_save')
-        message = opened_ticket.messages.latest()
-        assert message.sender_id == opened_ticket.requester_id
+        self.assertEqual(statuschangelog.before, '')
+        self.assertEqual(statuschangelog.after, Ticket.STATUS.new)
+        self.assertEqual(statuschangelog.changer_id, self.user.pk)
