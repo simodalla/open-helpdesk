43ce3958e31db57af5f9f72b7445d18d2d51cc8e
Simone Dalla <simodalla@gmail.com>
2014-08-04 15:55:49 +0200

    add display

--- tests/lives/test_django_live_client.py
+++ tests/lives/test_django_live_client.py
@@ -1,86 +1,58 @@
-# # -*- coding: utf-8 -*-
-# from __future__ import unicode_literals, absolute_import
-#
-# import pytest
-#
-# from django.contrib.admin.templatetags.admin_urls import admin_urlname
-# from django.test import LiveServerTestCase
-#
-# from selenium.webdriver.firefox.webdriver import WebDriver
-#
-# from openhelpdesk.models import Ticket, PRIORITY_NORMAL
-# from ..conftest import requester
-#
-#
-# @pytest.mark.livetest
-# class MySeleniumTests(LiveServerTestCase):
-#
-#     def get(self, url, *args, **kwargs):
-#         from django.core.urlresolvers import reverse
-#         if url.startswith('/'):
-#             return self.driver.get('{}{}{}'.format(
-#                 self.live_server_url,
-#                 url.rstrip('/'),
-#                 '/' if '?' not in url else ''), *args, **kwargs)
-#         return self.driver.get('{}{}'.format(
-#             self.live_server_url, reverse(url, args=args, kwargs=kwargs)))
-#
-#     def create_pre_authenticated_session(self, user=None):
-#         user = user or self.user
-#         from django.contrib.sessions.backends.db import SessionStore
-#         from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY
-#         from django.conf import settings
-#         session = SessionStore()
-#         session[SESSION_KEY] = user.pk
-#         session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
-#         session.save()
-#         # to set a cookie we need fo first visit the domain.
-#         # 404 pages load the quicktest! If is in testing of Mezzanine (>=3.1.9)
-#         # use an "admin" for bug on template_tags of mezzanine if app 'blog'
-#         # is not installed.
-#         self.driver.get(
-#             '{}/admin/404_no_such_url/'.format(self.live_server_url))
-#         self.driver.add_cookie(dict(name=settings.SESSION_COOKIE_NAME,
-#                                     value=session.session_key,
-#                                     path='/'))
-#         return session.session_key
-#
-#     @classmethod
-#     def setUpClass(cls):
-#         cls.driver = WebDriver()
-#         super(MySeleniumTests, cls).setUpClass()
-#
-#     @classmethod
-#     def tearDownClass(cls):
-#         cls.driver.quit()
-#         super(MySeleniumTests, cls).tearDownClass()
-#
-#     def setUp(self):
-#         self.user = requester()
-#         self.create_pre_authenticated_session()
-#
-#     def test_login(self):
-#         # pytest.set_trace()
-#         from ..conftest import get_tipologies
-#         tipologies = get_tipologies(2)
-#         self.get(admin_urlname(Ticket._meta, 'add'))
-#         for t in tipologies:
-#             self.driver.find_element_by_css_selector(
-#                 "#id_tipologies_from option[value='{}']".format(t.pk)).click()
-#             self.driver.find_element_by_css_selector(
-#                 '#id_tipologies_add_link').click()
-#         script = "tinyMCE.activeEditor.setContent('{}');".format("foo")
-#         self.driver.execute_script(script)
-#         self.driver.find_element_by_css_selector(
-#             "#id_priority input[value='{}']".format(PRIORITY_NORMAL)).click()
-#         self.driver.find_element_by_name('_save').click()
-#         ticket = Ticket.objects.latest()
-#         self.assertEqual(ticket.requester_id, self.user.id)
-#         self.assertEqual(ticket.priority, PRIORITY_NORMAL)
-#         self.assertIn("foo", ticket.content)
-#         self.assertSetEqual(
-#             set(ticket.tipologies.values_list('pk', flat=True)),
-#             {t.pk for t in tipologies})
-#
-#         # print(self.user.user)
-#
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, absolute_import
+
+import pytest
+
+from selenium import webdriver
+
+from django.contrib.auth import get_user_model
+
+from ..conftest import requester
+
+from django.contrib.sessions.backends.db import SessionStore
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY
+from django.conf import settings
+
+
+def create_pre_authenticated_session(driver, user, liveserver_url):
+    session = SessionStore()
+    session[SESSION_KEY] = user.pk
+    session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+    session.save()
+    # to set a cookie we need fo first visit the domain.
+    # 404 pages load the quicktest! If is in testing of Mezzanine (>=3.1.9)
+    # use an "admin" for bug on template_tags of mezzanine if app 'blog'
+    # is not installed.
+    driver.get(
+        '{}/admin/'.format(liveserver_url))
+    driver.add_cookie(dict(
+        name=settings.SESSION_COOKIE_NAME,
+        value=session.session_key,
+        path='/',
+    ))
+    return driver
+
+
+def test_live_live(live_server):
+    print(get_user_model().objects.values_list('pk', flat=True))
+    driver = webdriver.Firefox()
+    user = requester()
+    # create_pre_authenticated_session(driver, user, live_server.url)
+    print(get_user_model().objects.values_list('pk', flat=True))
+    # session = SessionStore()
+    # session[SESSION_KEY] = user.pk
+    # session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+    driver.get("http://www.python.org")
+    # driver.get('{}/admin/'.format(live_server.url))
+    driver.quit()
+
+
+def test_live_live_1(live_server):
+    print(get_user_model().objects.values_list('pk', flat=True))
+    driver = webdriver.Firefox()
+    user = requester()
+    # create_pre_authenticated_session(driver, user, live_server.url)
+    print(get_user_model().objects.values_list('pk', flat=True))
+    driver.get("http://www.python.org")
+    driver.get('{}/admin/'.format(live_server.url))
+    driver.quit()
