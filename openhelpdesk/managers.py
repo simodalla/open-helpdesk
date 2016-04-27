49ce15258d8651565979c50d0d5dc9f49ffadf8a
Simone Dalla <simodalla@gmail.com>
2016-04-27 17:23:07 +0200

    refactor OrganizationSettingManager

--- openhelpdesk/managers.py
+++ openhelpdesk/managers.py
@@ -3,29 +3,40 @@ from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
 
 
-class OrganizationSettingManager(models.Manager):
+class EmailOrganizationSettingManager(models.Manager):
 
-    def get_from_email(self, email):
+    def get(self, email):
         try:
             validate_email(email)
             domain = email.split('@')[1]
-            return self.get(email_domain=domain)
+            return super(EmailOrganizationSettingManager, self).get(
+                email_domain=domain)
         except Exception as e:
             raise e
 
-    def get_color_from_email(self, email):
+    def get_field(self, email, field):
         try:
             validate_email(email)
-            return self.get_fied_from_email(
+            obj = self.get(email)
+        except Exception as e:
+            raise e
+        return getattr(obj, field)
+
+    def get_color(self, email):
+        try:
+            validate_email(email)
+            return self.get_field(
                 email, 'email_background_color').lower().strip()
         except Exception:
             return ''
 
-    def get_fied_from_email(self, email, field):
+    def get_tipologies(self, email):
+        from .models import Tipology
         try:
-            validate_email(email)
-            obj = self.get_from_email(email)
+            obj = self.get(email)
+            return Tipology.objects.filter(
+                category__in=obj.categories.values_list('pk', flat=True))
         except Exception as e:
             raise e
-        return getattr(obj, field)
+
 
